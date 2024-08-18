b1edac18faafe41ca5224b13c371f13f01db835e
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2024-08-18 16:21:45 +0300

    сделала пока начало программы

---
+++ src/redax/store.js
@@ -0,0 +1,17 @@
+import { createStore } from "redux";
+import { devToolsEnhancer } from "@redux-devtools/extension";
+import { rootReducer } from "./reducer";
+
+{
+    contacts: {
+          items: []
+      },
+    filters: {
+          name: ""
+      }
+  }
+  const rootReducer = (state = initialState, action) => {
+    return state;
+  };
+  const enhancer = devToolsEnhancer();
+  export const store = createStore(rootReducer, enhancer);
